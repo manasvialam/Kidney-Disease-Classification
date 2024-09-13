79083d1704eef6d2889800fed791c377f2fc9bc9
manasvialam <manasvialam03@gmail.com>
2024-09-13 19:24:27 +0530

    data ingestion added

---
+++ src/cnnClassifier/entity/config_entity.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+@dataclass(frozen=True)
+class DataIngestionConfig:
+    rootdir: Path
+    source_URL : str
+    local_data_file : Path
+    unzip_dir: Path
